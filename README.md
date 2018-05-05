9c9a4da506b802f6f04993eb866188ba9809391e
Renato Alves <alves.rjc@gmail.com>
2018-05-05 18:05:26 +0200

    ENH Provide a simple installation script

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ In this repository you can find [external modules](http://ngless.embl.de/modules
 
 External modules are thin wrappers around different tools, making them convenient to integrate into NGLess scripts.
 
+## Install
+
+For a quick installation of all modules, clone this repository to some location on your computing servers and use the `install.sh` script provided.
+Symbolic links will be created in NGLess' user external module folder pointing to each module and version present in this repository.
+
 ## Contributing
 
 If you would like to have your favorite tool available in NGLess, [let us know](https://github.com/BigDataBiology/ngless_external_modules/issues) or open a [pull request](https://github.com/BigDataBiology/ngless_external_modules/pulls)
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Chdir to the current's script location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+INSTALLDIR="$HOME/.local/share/ngless/data/Modules"
+
+# Check that the user's ngless config dir exists
+mkdir -p "$INSTALLDIR"
+
+for mod in *.ngm; do
+    if [ ! -e "${INSTALLDIR}/$mod" ]; then
+        mkdir "${INSTALLDIR}/$mod"
+    fi
+    for version in ${mod}/* ; do
+        if [ ! -d "$version" ]; then
+            continue
+        fi
+        if [ -e "${INSTALLDIR}/${version}" ]; then
+            if [ -L "${INSTALLDIR}/${version}" ]; then
+                rm -f "${INSTALLDIR}/${version}"
+            else
+                echo "${INSTALLDIR}/${version} exists and is not a symlink. Please inspect and remove the folder manually. Skipping..." 
+                continue
+            fi
+        fi
+        echo "Installing ${version}" 
+        ln -s "$(pwd)/${version}" "${INSTALLDIR}/${version}" 
+    done
+done
